19fa98712f2f3e62115e5c9004771f5c115ac357
Adson Ventura <adsonventura@gmail.com>
2022-09-03 14:04:28 -0300

    Initial Commit

---
+++ assets/Icons/index.tsx
@@ -0,0 +1,9 @@
+import { Airplane } from "./Airplane";
+import { Building } from "./Building";
+import { Cocktail } from "./Cocktail";
+import { Earth } from "./Earth";
+import { Logo } from "./Logo";
+import { Museum } from "./Museum";
+import { Surf } from "./Surf";
+
+export { Airplane, Building, Cocktail, Earth, Logo, Museum, Surf };
